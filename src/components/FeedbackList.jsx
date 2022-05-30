6cd1c21710f425ef52ea515e97130d375faa44f7
Viktor Videnov <victor.valentinov.videnov@gmail.com>
2022-05-30 13:00:29 +0300

    Final Fixess

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,22 @@
 import FeedbackItem from "./FeedbackItem"
-
+import Spinner from './shared/Spinner'
 import { useContext } from 'react'
 import FeedbackContext from "../context/FeedbackContext"
 
 function FeedbackList() {
-    const { feedback } = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     }
 
-    return (
+    return isLoading ? < Spinner /> : (
         <div className="feedback-list">
             {feedback.map((item) => {
                 return <FeedbackItem
                     key={item.id}
                     item={item}
-                    />
+                />
             })}
         </div>
     )
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,52 +1,66 @@
-import { createContext, useState, useContent } from "react";
-import { v4 as uuidv4 } from 'uuid'
-
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item is Feedback 1',
-            rating: 9
-        },
-        {
-            id: 2,
-            text: 'This item is Feedback 2',
-            rating: 5
-        },
-        {
-            id: 3,
-            text: 'This item is Feedback 3',
-            rating: 4
-        }
-    ])
-
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    //State if something get wrong
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    //Fetch Feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
     //add Feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
     }
 
     //Delete Feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure want to delete?')) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE' })
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     //Update Feedback Item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
     }
 
-    //set item to be updated
+    //Set item to be updated
     const editFeedback = (item) => {
         setFeedbackEdit({
             item,
@@ -57,6 +71,7 @@ export const FeedbackProvider = ({ children }) => {
     return <FeedbackContext.Provider value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
